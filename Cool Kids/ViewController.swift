46205f32461f3d964673da4facd2854bca1ed77d
i2eslam <i52eslam@gmail.com>
2020-02-10 02:09:47 -0800

    Initial Commit

---
+++ Cool Kids/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cool Kids
+//
+//  Created by lodex solutions on 2/10/20.
+//  Copyright © 2020 ioslam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
