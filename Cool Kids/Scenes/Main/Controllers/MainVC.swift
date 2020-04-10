5f77f1f11ecb5bb2e428e2c97de04a6c9be642b6
eslam <i52eslam@gmail.com>
2020-04-10 06:15:19 +0200

    set splash loading by "Lottie" in MainVC

--- Cool Kids/Scenes/Main/Controllers/MainVC.swift
+++ Cool Kids/Scenes/Main/Controllers/MainVC.swift
@@ -7,28 +7,55 @@
 //
 
 import UIKit
+import Lottie
 
 class MainVC: UIViewController {
-
-    @IBAction func startBtnTapped(_ sender: UIButton) {
-        goToVC()
-    }
+    
+    @IBOutlet weak var splach_view: AnimationView!
+    @IBOutlet weak var startBtnTapped: UIButton!
+    @IBOutlet weak var splash_label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let frame_height = (self.view.frame.size.height)
-        Helper.saveUserDefaults(height: frame_height)
+        //MARK: - Invoke IBAction for the "start" Button
+        startBtnTapped.addTarget(self, action: #selector(goToVC), for: .touchUpInside)
+        //MARK: - save Device frame to jusify the tabBar Height by
+        Helper.saveUserDefaults(height: self.view.frame.size.height)
+        
+        //MARK: - Invoke Lottie Animation method
+        setLottieAnimation()
+        // hide all the layout when the lottie view is running
+        showHideSplashLayout(state: true, color: #colorLiteral(red: 0.2078431373, green: 0.2274509804, blue: 0.3137254902, alpha: 1))
+        //MARK: - Timer to show all layouts since lottie is loaded
+        Timer.scheduledTimer(withTimeInterval: 2, repeats: false) { (timer) in
+            self.showHideSplashLayout(state: false, color: #colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1))
+        }
     }
-
+    
 }
-
 extension MainVC {
-    func goToVC() {
+    //MARK: - if "start" button Pressed go to SuperVC
+    @objc func goToVC() {
         let SB = UIStoryboard(name: "Main", bundle: nil)
         let VC = SB.instantiateViewController(withIdentifier: "discoveryBar")
         VC.modalPresentationStyle = .fullScreen
         self.present(VC, animated: true)
     }
+    //MARK: -  Show & Hide IBoutlets by Lottie Loading
+    func showHideSplashLayout(state: Bool, color: UIColor) {
+        startBtnTapped.isHidden = state ? true : false
+        splash_label.isHidden = state ? true : false
+        splach_view.isHidden = state ? false : true
+        view.backgroundColor = color
+    }
+    //MARK: - declare lottie's settings
+    func setLottieAnimation() {
+        // set the lottie's json file-name
+        let animation = Animation.named("bus")
+        splach_view.animation = animation
+        splach_view.layer.cornerRadius = 25
+        splach_view.animationSpeed = 1
+        splach_view.play()
+    }
     
-    
-        
 }
